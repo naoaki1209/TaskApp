57ad30e6be58c6723b32d9f712cc8827444e1b35
vulcan999 <kaikakunote@gmail.com>
2017-02-18 11:43:03 +0900

    レッスン6の課題提出

---
+++ taskapp/Task.swift
@@ -0,0 +1,34 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 増澤尚翠 on 2017/02/12.
+//  Copyright © 2017年 vulcan999. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    /// 日時
+    dynamic var date = NSDate()
+    
+    // カテゴリ
+    dynamic var category = ""
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    
+}
